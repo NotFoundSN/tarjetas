db6688a0bfa467fad7b72137598ed118938b8f35
NotFoundSN <julio.maldonado.sn@gmail.com>
2024-02-28 18:45:30 -0300

    segundo commit

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -7,6 +7,7 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
+					<Route index element={<><div>web funcionando</div></>} />
 					<Route path="/invitation/:name" element={<Card />} />
 				</Route>
 			</Routes>
